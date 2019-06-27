e3377fe7c5c1b546d03acf4ced138424cbb48974
Rasmus Andersson <rasmus@notion.se>
2019-06-26 18:46:28 -0700

    distribution

--- dist.sh
+++ dist.sh
@@ -3,7 +3,34 @@ cd "$(dirname "$0")"
 
 ./build.js -O
 
-ZD=
-pushd "$ZD" >/dev/null
-zip -q -X -r "../../../$@" *
+echo "testing figplug by building examples"
+./bin/figplug build examples/basic
+./bin/figplug build examples/ui
+./bin/figplug build examples/ui-html
+
+echo "npm pack"
+pushd build >/dev/null
+rm -rf figplug-*.tgz package
+npm pack ..
+TAR_FILE=$(echo figplug-*.tgz)
+tar xzf "$TAR_FILE"
+
+pushd package >/dev/null
+ZIP_FILE=$(echo "$TAR_FILE" | sed 's/.tgz//g').zip
+zip -q -X -r "../$ZIP_FILE" *
 popd >/dev/null
+
+popd >/dev/null
+
+echo "created build/$ZIP_FILE"
+
+if [ -f "docs/dist/$ZIP_FILE" ]; then
+  echo "docs/dist/$ZIP_FILE already exists -- cowardly refusing to overwrite" >&2
+  exit 1
+fi
+
+cp -a "build/$ZIP_FILE" "docs/dist/$ZIP_FILE"
+
+echo ""
+echo "Remember to update the download link in docs/index.html"
+echo ""
