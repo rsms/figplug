e66db3928a78fd2f45c2780505195a57db6b65ad
Rasmus Andersson <rasmus@notion.se>
2019-08-07 18:19:54 -0700

    dist.sh updates

--- dist.sh
+++ dist.sh
@@ -4,35 +4,24 @@ cd "$(dirname "$0")"
 rm -rf build/*
 rm -f bin/figplug bin/figplug.map
 
+if ! (git diff-index --quiet HEAD --); then
+  echo "There are uncommitted changes:" >&2
+  git status -s --untracked-files=no --ignored=no
+  exit 1
+fi
+
 echo "building bin/figplug"
 ./build.js -O
 
 echo "testing figplug"
 ./test.sh
 
-
-# # Create package that can be installed with `npm install url`
-# pushd build >/dev/null
-# rm -rf figplug-*.tgz package
-# echo "npm pack"
-# if ! (npm pack .. > /dev/null 2>&1); then  # very noisy
-#   # repeat to print errors
-#   npm pack ..
-#   exit 1
-# fi
-# TAR_FILE=$(echo figplug-*.tgz)
-# popd >/dev/null  # back to .
-# DIST_FILE=fp-$(echo "$TAR_FILE" | sed 's/^figplug-//g')
-# if [ -f "docs/$DIST_FILE" ]; then
-#   echo "Package docs/$DIST_FILE already exists. You can manually replace it:" >&2
-#   echo "  cp -va 'build/$TAR_FILE' 'docs/$DIST_FILE'" >&2
-#   exit 1
-# fi
-# cp -va "build/$TAR_FILE" "docs/$DIST_FILE"
-
+VERSION=$(node -e 'process.stdout.write(require("./package.json").version)')
 
 echo ""
-echo "Finally, run npm publish when ready:"
-echo ""
-echo "npm publish ."
+echo "Next steps:"
+echo "1) Commit changes"
+echo "2) git tag v${VERSION} && git push --tags"
+echo "3) npm publish ."
+echo "4) Bump version in package.json"
 echo ""
